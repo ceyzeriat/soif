f5167567118ec7d0059bc374a2a4b7458b516595
Guillaume <guillaume.schworer@gmail.com>
2016-08-21 20:28:39 +0200

    tadaaaam

--- soif/test/test__core.py
+++ soif/test/test__core.py
@@ -34,7 +34,7 @@ def test_abs2(seed=42):
     x = np.random.rand(25)*100-50
     assert np.allclose(_core.abs2(x), np.power(np.abs(x), 2))
 
-def test_gen_seed(seed=42):
-    seeds = [_core.gen_seed(seed) for i in range(25)]
+def test_gen_seed():
+    seeds = [_core.gen_seed() for i in range(25)]
     assert len(seeds) == len(set(seeds))
 
