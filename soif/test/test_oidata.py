54f116ded9e38b618b832ed944747680cbe756fc
Guillaume <guillaume.schworer@gmail.com>
2016-09-10 20:16:40 +0200

    added test file

---
+++ soif/test/test_oidata.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  
+#  SOIF - Sofware for Optical Interferometry fitting
+#  Copyright (C) 2016  Guillaume Schworer
+#  
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  
+#  For any information, bug report, idea, donation, hug, beer, please contact
+#    guillaume.schworer@obspm.fr
+#
+###############################################################################
+
+
+import numpy as np
+import os
+
+from ..oidata import Oigrab, Oidata, OidataEmpty, Oifits
+
+FILENAME = os.path.dirname(os.path.abspath(__file__)) + '/MWC361.oifits'
+
+def test_oigrab():
+	oig = Oigrab(FILENAME)
+
+def test_oigrab_show_specs():
+	oig = Oigrab(FILENAME)
+	ans = oig.show_specs(ret=True)
+
